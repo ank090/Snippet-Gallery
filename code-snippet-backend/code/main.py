914db37784ed8968cd3656a388f7ed0c44326700
Ankit Verma <ankitver090@gmial.com>
2026-08-14 11:08:40 +0530

    added vercel domain in main.py

--- code-snippet-backend/code/main.py
+++ code-snippet-backend/code/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://snippet-gallery-git-main-ank090s-projects.vercel.app"],
+    allow_origins=["https://snippet-gallery.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
