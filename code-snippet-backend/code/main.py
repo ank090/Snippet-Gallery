80e2ef3f04e5602d22c6f1a30ab871650e0ab94c
Ankit Verma <ankitver090@gmial.com>
2026-08-14 10:17:16 +0530

    aaded vercel url in main.py

--- code-snippet-backend/code/main.py
+++ code-snippet-backend/code/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://snippet-gallery-g74lal8bs-ank090s-projects.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
